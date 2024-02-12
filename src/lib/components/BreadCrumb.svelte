d618b92a98462868d5e7edb437908bdc8002acfd
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-02-11 23:59:30 -0300

    asd

--- src/lib/components/BreadCrumb.svelte
+++ src/lib/components/BreadCrumb.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let actualSection: string;
 
 	const sections = [
@@ -24,10 +26,16 @@
 		}
 	];
 
-	const actualSectionIndex = sections.findIndex((e) => e.slug === actualSection);
+	let actualSectionIndex = -1; // Default value
+
+	// Extract the slug from the URL when component is mounted
+	onMount(() => {
+		const urlSlug = window.location.pathname.split('/').pop();
+		actualSectionIndex = sections.findIndex((e) => e.slug === urlSlug);
+	});
 </script>
 
-<section class="mb-24 mt-12 w-full px-4 py-4 lg:max-w-4xl">
+<section class="mb-24 mt-12 w-full p-4 lg:max-w-4xl">
 	<ul class="text-verde relative flex w-full items-center justify-between">
 		<span class="absolute left-0 top-2/4 h-0.5 w-full -translate-y-2/4 bg-white"></span>
 
@@ -35,11 +43,11 @@
 			class="absolute left-0 top-2/4 h-0.5 w-full -translate-y-2/4 border border-dashed border-green-900 bg-white transition-all duration-500"
 		></span>
 
-		{#each sections as section}
+		{#each sections as section, index}
 			<li
-				class="relative z-10 grid h-10 w-10 place-items-center rounded-full bg-white font-bold transition-all duration-300"
+				class={`relative z-10 grid h-12 w-12 place-items-center rounded-full bg-white text-2xl font-bold transition-all duration-300 ${index > actualSectionIndex ? 'bg-zinc-200' : 'shadow shadow-[#f8bc88]'}`}
 			>
-				{section.id + 1}
+				<span class={index > actualSectionIndex ? 'opacity-80' : ''}>{section.id + 1}</span>
 				<div class="absolute -bottom-[4.5rem] w-max text-center">
 					<h6
 						class="block font-sans text-base font-semibold leading-relaxed tracking-normal text-white antialiased"
--- src/routes/[token]/+layout.svelte
+++ src/routes/[token]/+layout.svelte
@@ -1,3 +1,3 @@
-<h1 class="text-dorado my-8 text-center text-4xl">Informe - DEP</h1>
+<h1 class="text-dorado my-8 text-center text-4xl font-bold">Informe - DEP</h1>
 
 <slot />
