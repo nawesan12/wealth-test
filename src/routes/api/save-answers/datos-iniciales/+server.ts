3d428441479b881cab8704b70c4f6ef07bbf7a8b
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-02-15 01:37:14 -0300

    asd

--- src/routes/api/save-answers/datos-iniciales/+server.ts
+++ src/routes/api/save-answers/datos-iniciales/+server.ts
@@ -11,15 +11,15 @@ export async function POST({ request }) {
 		}
 
 		const initialDataToUpload = {
-			name: data[0],
+			name: data[0] ?? '',
 			instagram: data[1].startsWith('@') ? data[1] : '@' + data[1],
-			web: data[2],
-			email: data[3],
-			whatsapp: data[4],
-			age: data[5],
-			country: data[6],
-			referredBy: data[7],
-			businessAgeAndSatisfaction: data[8]
+			web: data[2] ?? 'Sin Pagina Web',
+			email: data[3] ?? '',
+			whatsapp: data[4] ?? '',
+			age: data[5] ?? '',
+			country: data[6] ?? '',
+			referredBy: data[7] ?? '',
+			businessAgeAndSatisfaction: data[8] ?? ''
 		};
 
 		const uploadedUser = await prisma.user.create({
