0e4efb558bce98410d1a4ea8929e756b7f3f60c6
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2024-02-14 23:15:09 -0300

    asd

---
+++ src/routes/test/reglas/+page.svelte
@@ -0,0 +1,54 @@
+<main class="mx-auto flex min-h-screen flex-col items-center justify-center p-4 lg:max-w-3xl">
+	<h2 class="mb-8 text-center text-4xl font-semibold text-white">
+		¡Bienvenido/a al <span class="text-dorado">DEP!</span>
+	</h2>
+	<p class="mb-6 font-semibold text-white">
+		Te felicito por estar aquí. Para poder aprovechar al máximo este servicio y el tiempo que
+		dedicarás, te recomiendo que sigas estas reglas simples e importantes:
+	</p>
+
+	<article class="m-6 rounded-lg bg-[#FFFFFF11] p-4 text-lg text-white shadow-md">
+		<ul class="mb-6">
+			<li class="mb-4">
+				<span class="font-semibold">Compromiso y Honestidad:</span> Tómate tu tiempo para responder a
+				cada pregunta con sinceridad. No hay respuestas correctas o incorrectas.
+			</li>
+			<li class="mb-4">
+				<span class="font-semibold">Atención a los Detalles:</span> Lee cuidadosamente cada pregunta
+				y presta atención a las instrucciones que puedan acompañarla. A veces, encontrarás cuadros de
+				texto que explican conceptos importantes. Tómate el tiempo para entenderlos antes de responder.
+			</li>
+			<li class="mb-4">
+				<span class="font-semibold">Tiempo Suficiente:</span> Dedica el tiempo necesario para completar
+				el diagnóstico de manera adecuada. No hay prisa. Queremos que te sientas cómodo/a y seguro/a
+				con tus respuestas (el proceso completo podría demorar de 30’ a 45’).
+			</li>
+			<li class="mb-4">
+				<span class="font-semibold">Confidencialidad:</span> Tus respuestas son confidenciales. No compartiremos
+				tu información con terceros sin tu consentimiento.
+			</li>
+			<li class="mb-4">
+				<span class="font-semibold">Informe:</span> Al finalizar el DEP recibirás vía email un
+				informe con todas tus respuestas junto con los <b>BONUS</b> prometidos.
+			</li>
+		</ul>
+	</article>
+
+	<p class="my-4 font-semibold text-white">
+		Estoy muy agradecido por tu participación y espero que este proceso te brinde una mayor claridad
+		sobre tu negocio y te ayude a tomar decisiones más informadas en el futuro.
+	</p>
+	<p class="text-center text-2xl font-semibold text-white">
+		Gracias por confiar en <strong class="text-dorado font-semibold">Emperador Financiero.</strong>
+	</p>
+
+	<a class="bg-dorado text-verde my-8 rounded-lg px-6 py-4 font-bold" href="/test/datos-iniciales"
+		>Entendido, acceder al test</a
+	>
+</main>
+
+<style>
+	:global(body) {
+		overflow-y: auto;
+	}
+</style>
